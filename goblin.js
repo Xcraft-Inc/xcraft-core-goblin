ba663c90f25d441f57744fcba91e39322aac0afe
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-12-07 13:25:36 +0100

    Move static commands in a singleton (rename start to init)

--- goblin.js
+++ goblin.js
@@ -1,20 +1,10 @@
 'use strict';
 
-const Goblin = require('.');
-
 /**
  * Retrieve the list of available commands.
  *
  * @returns {Object} The list and definitions of commands.
  */
 exports.xcraftCommands = function() {
-  return {
-    handlers: Goblin.getCommands(),
-    rc: {
-      status: {
-        parallel: true,
-        desc: 'show the status of all goblins',
-      },
-    },
-  };
+  return require('./lib/service.js');
 };
