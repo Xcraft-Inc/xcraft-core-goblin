02833f6dd2391d5ad27e35429038fd49436d3ec0
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-05-02 23:27:03 +0200

    Add more tests (write side too)

--- test/elf.spec.js
+++ test/elf.spec.js
@@ -26,72 +26,136 @@ describe("Elf's spirit", function () {
   class TestState extends Elf.Sculpt(TestShape) {}
   let spirit = new TestState();
 
-  before(function () {
-    const plain = {
-      num: 42,
-      str: 'fourty two',
-      numArr: [10, 20, 30, 40],
-      strArr: ['one', 'two', 'three'],
-      obj: {
-        knight: 'Bragon',
-        princess: 'Mara',
-        master: 'Rige',
-      },
-    };
+  describe('read', function () {
+    before(function () {
+      const plain = {
+        num: 42,
+        str: 'fourty two',
+        numArr: [10, 20, 30, 40],
+        strArr: ['one', 'two', 'three'],
+        obj: {
+          knight: 'Bragon',
+          princess: 'Mara',
+          master: 'Rige',
+        },
+      };
 
-    const TestShredder = new Shredder(plain);
-    /* State based on a Shredder */
-    spirit = Elf.Spirit.from(TestState)(TestShredder);
+      const testShredder = new Shredder(plain);
+      testShredder._state = testShredder._state.asMutable();
+      spirit = Elf.Spirit.from(TestState)(testShredder);
+    });
+
+    it('one number', function () {
+      expect(spirit.num).to.be.equal(42);
+    });
+
+    it('one string', function () {
+      expect(spirit.str).to.be.equal('fourty two');
+    });
+
+    it('array number', function () {
+      expect(spirit.numArr[0]).to.be.equal(10);
+      expect(spirit.numArr[1]).to.be.equal(20);
+      expect(spirit.numArr[2]).to.be.equal(30);
+      expect(spirit.numArr[3]).to.be.equal(40);
+    });
+
+    it('array string', function () {
+      expect(spirit.strArr[0]).to.be.equal('one');
+      expect(spirit.strArr[1]).to.be.equal('two');
+      expect(spirit.strArr[2]).to.be.equal('three');
+    });
+
+    it('array length', function () {
+      expect(spirit.numArr).to.have.lengthOf(4);
+      expect(spirit.strArr).to.have.lengthOf(3);
+    });
+
+    it('for..of on array', function () {
+      let loop;
+
+      loop = 1;
+      for (const num of spirit.numArr) {
+        expect(num).to.be.equal(10 * loop++);
+      }
+
+      const results = ['one', 'two', 'three'];
+      loop = 0;
+      for (const num of spirit.strArr) {
+        expect(num).to.be.equal(results[loop++]);
+      }
+    });
+
+    it('for..of on object', function () {
+      for (const key of Object.keys(spirit.obj)) {
+        expect(key).to.be.oneOf(['knight', 'princess', 'master']);
+      }
+
+      for (const value of Object.values(spirit.obj)) {
+        expect(value).to.be.oneOf(['Bragon', 'Mara', 'Rige']);
+      }
+    });
   });
 
-  it('read one number', function () {
-    expect(spirit.num).to.be.equal(42);
-  });
+  describe('write', function () {
+    beforeEach(function () {
+      const plain = {
+        num: 0,
+        str: '',
+        numArr: [],
+        strArr: [],
+        obj: {
+          knight: '',
+          princess: '',
+          master: '',
+        },
+      };
 
-  it('read one string', function () {
-    expect(spirit.str).to.be.equal('fourty two');
-  });
+      const testShredder = new Shredder(plain);
+      testShredder._state = testShredder._state.asMutable();
+      spirit = Elf.Spirit.from(TestState)(testShredder);
+    });
 
-  it('read array number', function () {
-    expect(spirit.numArr[0]).to.be.equal(10);
-    expect(spirit.numArr[1]).to.be.equal(20);
-    expect(spirit.numArr[2]).to.be.equal(30);
-    expect(spirit.numArr[3]).to.be.equal(40);
-  });
+    it('one number', function () {
+      expect(spirit.num).to.be.equal(0);
+      spirit.num = 42;
+      expect(spirit.num).to.be.equal(42);
+    });
 
-  it('read array string', function () {
-    expect(spirit.strArr[0]).to.be.equal('one');
-    expect(spirit.strArr[1]).to.be.equal('two');
-    expect(spirit.strArr[2]).to.be.equal('three');
-  });
+    it('one string', function () {
+      expect(spirit.str).is.empty;
+      spirit.str = 'fourty two';
+      expect(spirit.str).to.be.equal('fourty two');
+    });
 
-  it('read array length', function () {
-    expect(spirit.numArr).to.have.lengthOf(4);
-    expect(spirit.strArr).to.have.lengthOf(3);
-  });
+    it('array number', function () {
+      spirit.numArr.push(10);
+      spirit.numArr.push(20);
+      spirit.numArr.push(30);
+      spirit.numArr.push(40);
+      expect(spirit.numArr[0]).to.be.equal(10);
+      expect(spirit.numArr[1]).to.be.equal(20);
+      expect(spirit.numArr[2]).to.be.equal(30);
+      expect(spirit.numArr[3]).to.be.equal(40);
+    });
 
-  it('read for..of on array', function () {
-    let loop;
+    it('array string', function () {
+      spirit.strArr.push('one');
+      spirit.strArr.push('two');
+      spirit.strArr.push('three');
+      expect(spirit.strArr[0]).to.be.equal('one');
+      expect(spirit.strArr[1]).to.be.equal('two');
+      expect(spirit.strArr[2]).to.be.equal('three');
+    });
 
-    loop = 1;
-    for (const num of spirit.numArr) {
-      expect(num).to.be.equal(10 * loop++);
-    }
+    it('array length', function () {
+      expect(spirit.numArr).to.have.lengthOf(0);
+      spirit.numArr.push(42);
+      expect(spirit.numArr).to.have.lengthOf(1);
 
-    const results = ['one', 'two', 'three'];
-    loop = 0;
-    for (const num of spirit.strArr) {
-      expect(num).to.be.equal(results[loop++]);
-    }
-  });
-
-  it('read for..of on object', function () {
-    for (const key of Object.keys(spirit.obj)) {
-      expect(key).to.be.oneOf(['knight', 'princess', 'master']);
-    }
-
-    for (const value of Object.values(spirit.obj)) {
-      expect(value).to.be.oneOf(['Bragon', 'Mara', 'Rige']);
-    }
+      expect(spirit.strArr).to.have.lengthOf(0);
+      spirit.strArr.push('fourty two');
+      expect(spirit.strArr).to.have.lengthOf(1);
+    });
   });
 });
