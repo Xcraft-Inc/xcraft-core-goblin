9f661ecf7fad2c9bef85b174f514aa1831a47fa9
Mathieu Schroeter <mathieu@schroetersa.ch>
2023-05-02 13:34:49 +0200

    Add more test cases

--- test/elf.spec.js
+++ test/elf.spec.js
@@ -13,6 +13,7 @@ describe("Elf's spirit", function () {
   class TestShape {
     num = number;
     str = string;
+    numArr = array(number);
     strArr = array(string);
   }
 
@@ -23,7 +24,8 @@ describe("Elf's spirit", function () {
     const plain = {
       num: 42,
       str: 'fourty two',
-      strArr: ['one', 'two', 'tree'],
+      numArr: [10, 20, 30, 40],
+      strArr: ['one', 'two', 'three'],
     };
 
     const TestShredder = new Shredder(plain);
@@ -39,7 +41,21 @@ describe("Elf's spirit", function () {
     expect(spirit.str).to.be.equal('fourty two');
   });
 
+  it('read array number', function () {
+    expect(spirit.numArr[0]).to.be.equal(10);
+    expect(spirit.numArr[1]).to.be.equal(20);
+    expect(spirit.numArr[2]).to.be.equal(30);
+    expect(spirit.numArr[3]).to.be.equal(40);
+  });
+
   it('read array string', function () {
     expect(spirit.strArr[0]).to.be.equal('one');
+    expect(spirit.strArr[1]).to.be.equal('two');
+    expect(spirit.strArr[2]).to.be.equal('three');
+  });
+
+  it('read array length', function () {
+    expect(spirit.numArr.length).to.be.equal(4);
+    expect(spirit.strArr.length).to.be.equal(3);
   });
 });
